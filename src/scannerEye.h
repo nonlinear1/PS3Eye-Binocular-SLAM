e6959cfc0a93061a4bb735251d4eb7b6301c650e
brenden m. peters <brenden.peters@gmail.com>
2018-08-26 19:03:14 -0400

    Readying for github push.

--- src/scannerEye.h
+++ src/scannerEye.h
@@ -3,9 +3,9 @@
 #include "ps3eye.h"
 #include "ps3eye_capi.h"
 
+//using namespace ps3eye;
 
-
-class scannerEye //: public PS3EYECam //ps3eye_t
+class scannerEye : public ps3eye::PS3EYECam 
 {
 	public:
 		void setup();
